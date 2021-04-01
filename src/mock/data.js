b8c2930b7f89406f27e38a7deec314c3e242ef17
jwang0110 <johnw7@uci.edu>
2021-04-01 00:21:20 -0700

    updated project info

--- src/mock/data.js
+++ src/mock/data.js
@@ -12,7 +12,7 @@ export const heroData = {
   title: 'Hello my name is',
   name: 'John Wang',
   subtitle: `I'm an Intermediate Developer.`,
-  cta: '',
+  cta: 'Learn More',
 };
 
 // ABOUT DATA
@@ -29,29 +29,29 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
+    title: 'Tango Land',
+    info: `Tango Land is a simple flashcard web application that test users on common Japanese expressions. This app lets users select which words they would like to study and how they want to study them. Users have the option to answer with the expression, kana, or romaji. It also shows additional information like pronunciation and definition as well as the score so that users can keep track of their progress.`,
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://tangoland.herokuapp.com',
+    repo: 'https://github.com/jwang0110/tangoland', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
+    title: 'ZTM Gatsby + Netlify CMS Starter',
+    info: `ZTM Gatsby + Netlify CMS Starter is a new up to date starter kit for a Gatsby.js blog. It connects to Netlify CMS to make it easier for users to create blog posts. On top of that, it has been built with Tailwind.css, a modern CSS framework that makes it easy for users to theme and customize their project. With the help of this starter kit, anyone can build their own personalized blog.`,
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/jwang0110/ztm-gatsby-netlify-cms-blog-starter', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
+    title: 'Fabflix',
+    info: `Fabflix is a fullstack web application that displays a list of movies from The Movie Database API. It offers features like searching and browsing to help users find movies they would be interested in. Users have the option of creating an account and bookmarking movies for future references.`,
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://fabflix.herokuapp.com',
+    repo: 'https://github.com/jwang0110/fabflix', // if no repo, the button will not show up
   },
 ];
 
