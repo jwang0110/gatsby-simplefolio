8589a92b98fc1813f40d5d43ce33903897ec3124
jwang0110 <johnw7@uci.edu>
2021-04-05 21:25:42 -0700

    added project images

--- src/mock/data.js
+++ src/mock/data.js
@@ -27,7 +27,7 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
+    img: 'tangoland.png',
     title: 'Tango Land',
     info: `Tango Land is a simple flashcard web application that test users on common Japanese expressions. This app lets users select which words they would like to study and how they want to study them. Users have the option to answer with the expression, kana, or romaji. It also shows additional information like pronunciation and definition as well as the score so that users can keep track of their progress.`,
     info2: '',
@@ -36,7 +36,7 @@ export const projectsData = [
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
+    img: 'gatsbyblog.png',
     title: 'ZTM Gatsby + Netlify CMS Starter',
     info: `ZTM Gatsby + Netlify CMS Starter is a new up to date starter kit for a Gatsby.js blog. It connects to Netlify CMS to make it easier for users to create blog posts. On top of that, it has been built with Tailwind.css, a modern CSS framework that makes it easy for users to theme and customize their project. With the help of this starter kit, anyone can build their own personalized blog.`,
     info2: '',
@@ -45,7 +45,7 @@ export const projectsData = [
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
+    img: 'fabflix.png',
     title: 'Fabflix',
     info: `Fabflix is a fullstack web application that displays a list of movies from The Movie Database API. It offers features like searching and browsing to help users find movies they would be interested in. Users have the option of creating an account and bookmarking movies for future references.`,
     info2: '',
@@ -79,5 +79,5 @@ export const footerData = {
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
